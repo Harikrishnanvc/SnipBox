f5001d0e365714791b687ef932cccd1ee2e90549
hari <hari154542@gmail.com>
2025-02-03 00:08:27 +0530

    Add DELETE /api/snippets/id/ endpoint for deleting selected snippets

--- snippets/views.py
+++ snippets/views.py
@@ -27,4 +27,23 @@ class SnippetView(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def destroy(self, request, *args, **kwargs):
+        snippet_ids = request.data.get('ids', [])
+        user = self.request.user
+        snippets = Snippets.objects.filter(user=user, id__in=snippet_ids)
+
+        if not snippets.exists():
+            return Response({"detail": "No matching snippets found."}, status=status.HTTP_404_NOT_FOUND)
+
+        snippets.delete()
+
+        updated_snippets = Snippets.objects.filter(user=user)
+        serialized_snippets = SnippetsSerializer(updated_snippets, many=True).data
+        response_data = {
+            "total_snippets": updated_snippets.count(),
+            "snippets": serialized_snippets
+        }
+        return Response(response_data, status=status.HTTP_204_NO_CONTENT)
+
+
 
